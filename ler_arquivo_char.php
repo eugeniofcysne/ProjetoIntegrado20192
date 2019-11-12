8ed23f63cce101d5efce75a702d72cb57348779b
Eugênio <eugeniofcysne@gmail.com>
2019-11-12 20:03:55 -0300

    char funcionando
    char funcionando.

--- ler_arquivo_char.php
+++ ler_arquivo_char.php
@@ -13,7 +13,6 @@ if ($num_reg > 0) {
 }
 
 $texto_char = $_POST['texto_char'];
-echo $texto_char;
 $tamanho_total = strlen($texto_char);
 $texto_array = str_split($texto_char);
 $texto_final = array();
@@ -21,27 +20,61 @@ $teste = false;
 $posicao = 0;
 $cebola = '';
 $num32 = 32;
+$texto_auxiliar = '';
 
 while ($teste == false) {
-    $cebola = $texto_array[$posicao] . $texto_array[$posicao + 1] . $texto_array[$posicao + 2] . $texto_array[$posicao + 3] . $texto_array[$posicao + 4];
-    //ler posiçao [0]
-    if ($cebola == "char(") {
-        //ler posição [1];
-        
-        if (intval($texto_array[$posicao + 5]) != 2 && intval($texto_array[$posicao + 6]) !=0) {
-            if (intval($texto_array[$posicao + 6]) >=0 && intval($texto_array[$posicao+6])<=2) {
-                echo "chegou na segunda posicao";
-                //   traduz da tabela
-                $texto3pos = "{$texto_array[$posicao]}{$texto_array[$posicao + 1]}{$texto_array[$posicao + 2]}{$texto_array[$posicao + 3]}{$texto_array[$posicao + 4]}{$texto_array[$posicao + 5]}{$texto_array[$posicao + 6]}";
+    if ($posicao <= ($tamanho_total - 5)) {
+        $cebola = $texto_array[$posicao] . $texto_array[$posicao + 1] . $texto_array[$posicao + 2] . $texto_array[$posicao + 3] . $texto_array[$posicao + 4];
+        //ler posiçao [0]
+        if ($cebola == "char(") {
 
-                foreach ($tabelaTemp as $key => $value) {
+            //ler posição [1];
 
-                    if ($key == $texto3pos) {
+            if (intval($texto_array[$posicao + 5]) != 2 && intval($texto_array[$posicao + 6]) != 0) {
 
-                        array_push($texto_final, $value);
-                        $posicao = $posicao + 3;
-                        break;
+                if (intval($texto_array[$posicao + 6]) >= 0 && intval($texto_array[$posicao + 6]) < 10) {
+                    //ATÉ AQUI TA BELEZA
+                    if ($texto_array[$posicao + 7] == ")") {
+                        $texto3pos = "{$texto_array[$posicao]}{$texto_array[$posicao + 1]}{$texto_array[$posicao + 2]}{$texto_array[$posicao + 3]}{$texto_array[$posicao + 4]}{$texto_array[$posicao + 5]}{$texto_array[$posicao + 6]}{$texto_array[$posicao + 7]}";
+
+                        //  traduz da tabela
+                        foreach ($tabelaTemp as $key => $value) {
+
+                            if ($key == $texto3pos) {
+
+                                array_push($texto_final, $value);
+                                $posicao = $posicao + 8;
+                                break;
+                            }
+                        }
+                    } else if (intval($texto_array[$posicao + 7]) >= 0 && intval($texto_array[$posicao + 7]) <= 9) {
+                        $texto_auxiliar = "{$texto_array[$posicao + 5]}{$texto_array[$posicao + 6]}{$texto_array[$posicao + 7]}";
+                        if (intval($texto_auxiliar <= 126)) {
+                            $texto3pos = "{$texto_array[$posicao]}{$texto_array[$posicao + 1]}{$texto_array[$posicao + 2]}{$texto_array[$posicao + 3]}{$texto_array[$posicao + 4]}{$texto_array[$posicao + 5]}{$texto_array[$posicao + 6]}{$texto_array[$posicao + 7]}{$texto_array[$posicao + 8]}";
+
+                            //  traduz da tabela
+                            foreach ($tabelaTemp as $key => $value) {
+
+                                if ($key == $texto3pos) {
+
+                                    array_push($texto_final, $value);
+                                    $posicao = $posicao + 9;
+                                    break;
+                                }
+                            }
+                        } else {
+                            array_push($texto_final, $texto_array[$posicao]);
+                            $posicao++;
+                        }
+                        //   traduz da tabela
+
+                    } else {
+                        array_push($texto_final, $texto_array[$posicao]);
+                        $posicao++;
                     }
+                } else {
+                    array_push($texto_final, $texto_array[$posicao]);
+                    $posicao++;
                 }
             } else {
                 array_push($texto_final, $texto_array[$posicao]);
@@ -51,13 +84,15 @@ while ($teste == false) {
             array_push($texto_final, $texto_array[$posicao]);
             $posicao++;
         }
+        if ($posicao >= $tamanho_total) {
+            $teste = true;
+        }
     } else {
         array_push($texto_final, $texto_array[$posicao]);
         $posicao++;
-    }
-
-    if ($posicao >= $tamanho_total) {
-        $teste = true;
+        if ($posicao >= $tamanho_total) {
+            $teste = true;
+        }
     }
 }
 $str = implode("", $texto_final);
